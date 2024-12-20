c011d9bc16cf0d2328dac75b2debfd7fee69ee1a
Paul King <paulk@asert.com.au>
2024-12-20 12:49:41 +1000

    Add macro for historical links #230 (can't have one macro that is the suffix of another)

--- doc/asciidoctor-extension/src/main/groovy/geb/doc/asciidoctor/extension/BookOfGebExtension.groovy
+++ doc/asciidoctor-extension/src/main/groovy/geb/doc/asciidoctor/extension/BookOfGebExtension.groovy
@@ -27,7 +27,7 @@ class BookOfGebExtension implements ExtensionRegistry {
     void register(Asciidoctor asciidoctor) {
         asciidoctor.javaExtensionRegistry()
             .inlineMacro("issue", IssueLinkMacro)
-            .inlineMacro("hissue", HistoricalIssueLinkMacro)
+            .inlineMacro("issueh", HistoricalIssueLinkMacro)
             .inlineMacro("github-profile", GithubProfileLinkMacro)
     }
 
